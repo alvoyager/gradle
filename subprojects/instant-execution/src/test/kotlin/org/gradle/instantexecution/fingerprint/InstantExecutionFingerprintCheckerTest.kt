eb93bb64aeff30301bb30743517117778755978c
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-03-14 16:29:47 -0300

    Invalidate instant execution cache when a `buildSrc` task input directory changes

--- subprojects/instant-execution/src/test/kotlin/org/gradle/instantexecution/fingerprint/InstantExecutionFingerprintCheckerTest.kt
+++ subprojects/instant-execution/src/test/kotlin/org/gradle/instantexecution/fingerprint/InstantExecutionFingerprintCheckerTest.kt
@@ -70,6 +70,7 @@ class InstantExecutionFingerprintCheckerTest {
             InstantExecutionFingerprintChecker.FingerprintEncoder.run {
                 encode(
                     InstantExecutionCacheFingerprint(
+                        taskInputs = emptyList(),
                         inputFiles = emptyList(),
                         obtainedValues = listOf(obtainedValue)
                     )
